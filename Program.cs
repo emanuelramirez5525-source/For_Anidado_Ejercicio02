0b417731c879541e9bf8ef032585c6cb880b4045
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 12:57:24 -0600

    Logica aplicada correctamente!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿// Mostrar un rectángulo de 4 filas y 6 columnas con asteriscos.
+int filas = 6;
+int columnas = 4;
+for (int i = 1; i<= filas; i++)
+{
+    for (int j = 1; j<= columnas; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
